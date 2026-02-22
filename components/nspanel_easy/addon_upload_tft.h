7d847b9a3d35d815a9adedc55b54e6828d3da9fe
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-02-22 01:28:43 +0100

    Fix nextion_update_base_url and stop BT components before upload

--- components/nspanel_easy/addon_upload_tft.h
+++ components/nspanel_easy/addon_upload_tft.h
@@ -10,6 +10,8 @@ namespace nspanel_easy {
 
     // TFT upload state variables (previously YAML globals)
     extern uint8_t tft_upload_attempt;
+    extern bool tft_upload_manual_request;
+    extern uint32_t tft_upload_first_time_synced_ms;
     extern bool tft_upload_result;
 
 }  // namespace nspanel_easy
